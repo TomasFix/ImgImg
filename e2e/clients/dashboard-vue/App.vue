95ce2a422de283825cf4c77cc067147ff11ac145
TomasFix <hokatandai@gmail.com>
2024-02-29 11:41:01 +0300

    Adding feature for e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :ImageTestVarLang="ImageTestVarLang" />
+</template>
+
+<script>
+import Vue from 'vue'
+import ImageTestVarLang from '@ImageTestVarLang/core'
+import { Dashboard } from '@ImageTestVarLang/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    ImageTestVarLang: () => new ImageTestVarLang(),
+  },
+}
+</script>
+
+<style src="@ImageTestVarLang/core/dist/style.css"></style>
+<style src="@ImageTestVarLang/dashboard/dist/style.css"></style>

75130b51a4f3e5ac14e920bc619971f1d5359c67
TomasFix <hokatandai@gmail.com>
2024-04-17 12:30:01 +0300

    Update e2e/mock-server.mjs

---
+++ e2e/mock-server.mjs
@@ -0,0 +1,28 @@
+import http from 'node:http'
+
+const requestListener = (req, res) => {
+  const endpoint = req.url
+
+  switch (endpoint) {
+    case '/file-with-content-disposition': {
+      const fileName = `DALL·E IMG_9078 - 学中文 🤑`
+      res.setHeader('Content-Disposition', `attachment; filename="ASCII-name.zip"; filename*=UTF-8''${encodeURIComponent(fileName)}`)
+      res.setHeader('Content-Type', 'image/jpeg')
+      res.setHeader('Content-Length', '86511')
+      break
+    }
+    case '/file-no-headers':
+      break
+    default:
+      res.writeHead(404).end('Unhandled request')
+  }
+
+  res.end()
+}
+export default function startMockServer (host, port) {
+  const server = http.createServer(requestListener)
+  server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`)
+  })
+}
+
